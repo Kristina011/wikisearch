e48487e1e75f827ad45a81d193920948811f995a
kikiBg <kristinarsic@gmail.com>
2020-04-16 21:05:09 +0200

    SRV001 set pages and security

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import { WikipediaService } from './wikipedia.service';
 })
 export class AppComponent {
   constructor(private wikipedia: WikipediaService) {}
+  pages = [];
 
   onTerm(term: string) {
-   this.wikipedia.search(term).subscribe((response) => {
-      console.log(response);
+   this.wikipedia.search(term).subscribe((response: any) => {
+     this.pages = response.query.search;
    });
   }
 }
